cfd99bb5f9ad180037eec46588f31ff0c672317d
Helen Lim <hsl64@cornell.edu>
2025-02-07 04:03:29 -0500

    fixed setup

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import *
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="static/", index_file="index.html", autorefresh=True)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="/", index_file="index.html", autorefresh=True)
 
 #  note - you can change the prefix above and get rid of this route if you just want to host static
-#  @app.route('/', methods=['GET'])
-#  def hello():
+#@app.route('/', methods=['GET'])
+#def hello():
 #    return make_response("testing~")
 
 if __name__ == "__main__":
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=9000)
